da9b8a09f64494f5ba902027f1024215e29ce9a0
addwater1 <shokuhou93@gmail.com>
2024-06-06 16:55:31 +0900

    Hello world with react

--- report-front-end/src/App.js
+++ report-front-end/src/App.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import "./style.css";
 
 export const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className='container'>
-      <h1>Hello world!</h1>
+      <h1>Hello world! {count}</h1>
+      <button 
+        onClick={() => {
+          setCount(c => c+1);
+        }}
+      >
+        Count
+      </button>
     </div>
   );
 };
